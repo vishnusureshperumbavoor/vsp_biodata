bf600bf5677acb10b8007b2b294d4c6513895af0
vishnusureshperumbavoor <vishnusureshperumbavoor@gmail.com>
2023-04-28 17:38:13 +0530

    added changes

---
+++ node_modules/vsp-biodata/index.js
@@ -0,0 +1,16 @@
+const biodata = {
+    name: "Vishnu Suresh",
+    dob: "4/10/1999",
+    occupation: "AI Engineer",
+    interests: ["building companies", "flirting"],
+    futureplans : {
+        1: "build a company",
+        2: "contribute to artificial general intelligence",
+        3: "destroy all the human jobs",
+        4: "make bitcoin the currency of earth",
+        5: "make India a world superpower",
+        6: "make humanity a multiplanetary species"
+    },
+  };
+
+module.exports = biodata;
